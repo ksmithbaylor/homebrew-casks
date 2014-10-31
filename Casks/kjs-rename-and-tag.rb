172fb93d66c89ce4c21713c71f527a87ebd9f187
Kevin Smith <ksmithbaylor@gmail.com>
2014-10-31 10:38:43 -0400

    added my rename and tag app

---
+++ Casks/kjs-rename-and-tag.rb
@@ -0,0 +1,10 @@
+class KjsRenameAndTag < Cask
+  version '1.0'
+  sha256 '238738d472df878b3d4f1540a238f5fcfb97a93fa1b3c8ae24cc315cd20f998b'
+
+  url 'https://github.com/ksmithbaylor/homebrew-casks/blob/master/files/rename-and-tag.zip?raw=true'
+  homepage 'https://github.com/ksmithbaylor/homebrew-casks/tree/master/files'
+  license :unknown
+
+  app 'Rename and Tag.app'
+end

6d7d9fa8a72ed9fb3800d6368920d08d0327e250
Kevin Smith <ksmithbaylor@gmail.com>
2014-10-30 21:38:16 -0400

    added junos pulse cask

---
+++ Casks/kjs-junos-pulse.rb
@@ -0,0 +1,10 @@
+class KjsJunosPulse < Cask
+  version '5.0.4'
+  sha256 '7e8c40dacdbf99ea16cd39f797c339e623fa5710f17453b71dedbef6db746c90'
+
+  url 'https://github.com/ksmithbaylor/homebrew-casks/blob/master/files/junos-pulse.zip?raw=true'
+  homepage 'https://github.com/ksmithbaylor/homebrew-casks/tree/master/files'
+  license :unknown
+
+  app 'Junos Pulse.app'
+end
